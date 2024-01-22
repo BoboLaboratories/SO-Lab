d0b211447a3ee422da5a3bdc6964feb627eddfd4
Fabio Nebbia <fabionebbia97@gmail.com>
2024-01-22 20:40:48 +0100

    alabulai

--- env/base.sh
+++ env/base.sh
@@ -7,11 +7,7 @@ export N_ATOM_MAX=300
 export MIN_N_ATOMICO=200
 
 export STEP_ATTIVATORE=25000000 # nanoseconds
-<<<<<<< HEAD
-export STEP_ALIMENTAZIONE=250000000 # nanoseconds
-=======
 export STEP_ALIMENTAZIONE=25000000 # nanoseconds
->>>>>>> a5876a3 (cerchiamo di capire)
 
 export ENERGY_DEMAND=1500
 export ENERGY_EXPLODE_THRESHOLD=40000
